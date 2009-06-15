5ba060dbaad8b30561464c7918ad7ace1b44a4a3
Duncan Hawthorne <Duncan Hawthorne@localhost>
2009-06-15 12:46:55 +0100

    generalise wallpaper size, repeat album images when there are less than necessary to fill wallpaper

--- valkyrie.py
+++ valkyrie.py
@@ -5,45 +5,67 @@ import pylast
 import getpass
 import os
 import urllib
-import glob
+import hashlib
+import sys
 
+def hcf(a,b):
+	while b != 0:
+		remainder = a % b
+		a = b
+		b = remainder
+	return a
 
-API_KEY = 'eda6d854f8f3d5a03da8fa4960f687fe'
-API_SECRET = 'c2099b3b7500d1a9fd8dace8b89b76fe'
-username = raw_input("Please enter your username: ")
-#username = 'timskinner1' #FIXME
-md5_password = pylast.md5(getpass.getpass())
-session_key = pylast.SessionKeyGenerator(API_KEY, API_SECRET).get_session_key(username, md5_password)
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "run like\npython valkyrie.py your_screen_width your_screen_height"
+		quit()
+	else: #all good
+		screen_width = int(sys.argv[1])
+		screen_height = int(sys.argv[2])
 
-user = pylast.User(username, API_KEY, API_SECRET, session_key)
-fetched_albums = {}
-periods = ['6month', '3month', '12month', 'overall']
-for period in periods:
-	new_albums = user.get_top_albums(period)
-	for alb in new_albums:
-		if alb.get_item().get_title() not in fetched_albums:
-			fetched_albums[alb.get_item().get_title()] = alb.get_item()
+	API_KEY = 'eda6d854f8f3d5a03da8fa4960f687fe'
+	API_SECRET = 'c2099b3b7500d1a9fd8dace8b89b76fe'
+	username = raw_input("Please enter your username: ")
+	#username = 'timskinner1' #FIXME
+	md5_password = pylast.md5(getpass.getpass())
+	session_key = pylast.SessionKeyGenerator(API_KEY, API_SECRET).get_session_key(username, md5_password)
 
-homedir = os.path.expanduser('~')
-if not os.path.exists(homedir + '/.cache/valkyrie'):
-	os.mkdir(homedir + '/.cache/valkyrie')
-os.chdir(homedir + '/.cache/valkyrie')
-os.system('rm -- *.jpg')
+	user = pylast.User(username, API_KEY, API_SECRET, session_key)
+	print("connection established, looking for album art")
 
-jpg_count = 0
-for key in fetched_albums:
-	alb = fetched_albums[key]
-	if jpg_count < 80:
-		try :
-			url = alb.get_image_url()
-			if url != None:
-				urllib.urlretrieve(url, str(hash(alb.get_title())) + '.jpg')
-				jpg_list = glob.glob("*.jpg")
-				jpg_count = len(jpg_list)
-			else:		
-				print(alb.get_name() + ": artwork not found")
-		except:
-			None
+	fetched_albums = {}
+	periods = ['6month', '3month', '12month', 'overall']
+	for period in periods:
+		new_albums = user.get_top_albums(period)
+		for alb in new_albums:
+			if alb.get_item().get_title() not in fetched_albums:
+				fetched_albums[alb.get_item().get_title()] = alb.get_item()
 
-os.system('montage -mode Concatenate -resize 128x128! -tile 10x8 *.jpg ~/test.jpg')
+	homedir = os.path.expanduser('~')
+	if not os.path.exists(homedir + '/.cache/valkyrie'):
+		os.mkdir(homedir + '/.cache/valkyrie')
+	os.chdir(homedir + '/.cache/valkyrie')
+	os.system('rm -- *.jpg')
+
+	size = hcf(screen_width,screen_height)
+	while size >= 160:
+		size = size/2 #FIXME fails if not divisible by 2, but that never happens
+	#now 80 <= size < 160
+
+	jpg_count = 0
+	while jpg_count < (screen_width/size)*(screen_height/size): #so loops around if less than necessary number of albums
+		for key in fetched_albums:
+			alb = fetched_albums[key]
+			if jpg_count < (screen_width/size)*(screen_height/size):
+				try :
+					url = alb.get_image_url()
+					if url != None:
+						urllib.urlretrieve(url, hashlib.md5(alb.get_title()+str(jpg_count)).hexdigest() + '.jpg')#insert jpg_count so filename changes on each while for same album
+						jpg_count += 1
+					else:		
+						print(alb.get_name() + ": artwork not found")
+				except:
+					None
+
+	os.system('montage -mode Concatenate -resize '+str(size)+'x'+str(size)+'! -tile '+str(screen_width/size)+'x'+str(screen_height/size)+' *.jpg ~/test.jpg')
 
